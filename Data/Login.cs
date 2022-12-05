3f629de250984cd8257bc5e93b30514df1213853
And3rsLMM <andersonlimam.al@gmail.com>
2022-12-05 06:10:38 -0300

    primeiro commit

---
+++ Data/Login.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using Newtonsoft.Json;
+
+namespace VoeAirlinesBlazor.Data
+{
+    public class Login
+    {
+        [JsonProperty("Usuario")]
+
+        public string? Usuario { get; set; }
+
+        [JsonProperty("DataCriacao")]
+
+        public DateTime DataCriacao { get; set; }
+
+        [JsonProperty("Senha")]
+
+        public string? Senha { get; set; }
+
+    }
+}
